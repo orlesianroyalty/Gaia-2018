e4eadf380aba02c0afa760233be8dda38504761c
amodek <34949830+amodek@users.noreply.github.com>
2018-03-20 13:47:18 -0500

    Add files via upload

---
+++ Assets/Scripts/AbilityFireball.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Goes on player as a script component
+ * Requirements: 
+ * Assign the fireball GameObject
+ * Put 'ProjectileFireball' script on fireball GameObject */
+
+public class AbilityFireball : MonoBehaviour {
+
+	public KeyCode fireKey = KeyCode.T;  //set trigger key for fireball 
+	public GameObject fireball; //assign fireball object, make sure you add 'ProjectileFireball' as a script component to it
+
+	private Camera cam; 
+	private Vector3 view, position;  
+	private GameObject editedFireball; 
+
+	//for cooldown timer
+	private bool cooldownNeeded = false; 
+	private int cooldownTime = 5; 
+	private float timePassed = 15; 
+
+
+	//every frame 
+	void Update () {
+		if (cooldownNeeded) { 
+			timePassed += Time.deltaTime; 
+		} 
+
+		if (timePassed > cooldownTime) {
+			cooldownNeeded = false; 
+			makeFireball();
+		}
+	}
+
+
+	/**Launches a fireball depending on how long the user holds down 
+	 * the trigger key. 'FireballProjectile' script must be a component of 
+	 * 'fireball' object for this to work.  */
+	void makeFireball() { 
+		if (Input.GetKeyDown (KeyCode.T)) {  
+			Vector3 here = createLaunch (); 
+			editedFireball = Instantiate (fireball, here, Quaternion.identity);
+			editedFireball.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+
+			//for cooldown timer
+			cooldownNeeded = true; 
+			timePassed = 0; 
+		} 
+
+	} 
+
+	// creates launch point for fireball
+	public Vector3 createLaunch() {
+		float playerX; 
+		float playerY = gameObject.transform.position.y + 0.25f; 
+		Vector3 launch;
+
+		if (!facingLeft ()) { 
+			playerX = gameObject.transform.position.x + 1.5f; 
+			launch = new Vector3 (playerX, playerY,
+				gameObject.transform.position.z);	
+			return launch; 
+		} 
+		playerX = gameObject.transform.position.x - 1.5f; 
+		launch = new Vector3 (playerX, playerY,   
+			gameObject.transform.position.z);	
+		return launch; 
+
+	}
+
+
+	/** returns:
+	 * true if player is facing left 
+	 * false if player is facing right 
+	 * */
+	bool facingLeft() { 
+		if (gameObject.transform.localScale.x > 0) { 
+			return false; 
+		} 
+		return true; 
+	}
+
+		
+}
---
+++ Assets/Scripts/ProjectileFireball.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*Goes on fireball object, you guys will have to specify how much damage you want this to cause, 
+ * do this under method 'damageEnemy()' in this script */
+public class ProjectileFireball : MonoBehaviour {
+
+	private Camera cam; 
+	private float facing; 
+	private bool fired = false; 
+	private GameObject player; 
+	private Vector3 view, position; 
+	private float speed = 20f; 
+	private float size, elapsedTime; 
+
+
+	void Start() {
+		facing = 0; 
+		size = 0.5f; 
+		elapsedTime = 0.0f; 
+		cam = Camera.main; 
+		player = GameObject.Find ("Player"); 
+	} 
+
+
+	//every frame
+	void Update() {
+		shootFireball (); 
+		if (!isInView ()) { 
+			damageEnemy (); 
+			Destroy (gameObject); 
+		} 
+	} 
+
+	//charges up the fireball 
+	void shootFireball() { 
+		if (!fired) { 
+			if (Input.GetKey (KeyCode.T) && elapsedTime < 2.0f) { 
+				elapsedTime += Time.deltaTime; 
+				size += (elapsedTime / 100.0f); 
+				gameObject.transform.position = createLaunch (); 
+				gameObject.transform.localScale = new Vector3 (size, size, size); 
+			}
+			if (Input.GetKeyUp (KeyCode.T) || elapsedTime > 2.0f) { 
+				facing = player.transform.localScale.x; 
+				fired = true;  
+			}
+		} 
+		if ( facing > 0) { 
+			transform.Translate (Vector3.right * speed * Time.deltaTime, relativeTo: Space.World);
+		} 
+		if ( facing < 0) { 
+			transform.Translate (Vector3.left * speed * Time.deltaTime, relativeTo: Space.World); 
+		}
+	} 
+
+
+	/*Returns true if object is in view of main camera
+	 *if not, returns false
+	 * */
+	bool isInView() { 
+		position = gameObject.transform.position; 
+		view = cam.WorldToViewportPoint (position); 
+		if (view.x > 1.3 || view.x < -0.3) { 
+			return false;
+		} 
+		return true;
+	} 
+
+
+		// creates launch point for fireball
+		public Vector3 createLaunch() {
+			float playerX; 
+			float playerY = player.transform.position.y + 0.25f; 
+			Vector3 launch;
+
+			if (!facingLeft ()) { 
+				playerX = player.transform.position.x + 1.5f; 
+				launch = new Vector3 (playerX, playerY,
+					player.transform.position.z);	
+				return launch; 
+			} 
+			playerX = player.transform.position.x - 1.5f; 
+			launch = new Vector3 (playerX, playerY,   
+				player.transform.position.z);	
+			return launch; 
+
+		}
+
+
+		/** returns
+	  * true if facing left 
+	  * false if facing right 
+	  * */
+		bool facingLeft() { 
+			if (player.transform.localScale.x > 0) { 
+				return false; 
+			} 
+			return true; 
+		}
+
+
+		/*damages enemy based on size of fireball 
+		 * */
+		void damageEnemy() { 
+		    if (gameObject.transform.localScale.x >= 1.5f) { 
+				//script to damage enemy maximum amount
+				return; 
+			} 
+			if (gameObject.transform.localScale.x >= 0.75) { 
+			//script to damage enemy medium amount
+			return; 
+			}
+			//script to damage enemy minimum amount
+		} 
+
+	}  
